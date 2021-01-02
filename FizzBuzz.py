da71934b6973a3a5543377af160c7ea2cc1252e6
Danish <57083546+Danish-1@users.noreply.github.com>
2021-01-03 00:47:32 +0530

    My commit

---
+++ FizzBuzz.py
@@ -0,0 +1,16 @@
+# take an input
+n = int(input())
+# loop from 1 to n odd numbers
+for x in range(1, n, 2):
+    if x % 3 == 0 and x % 5 == 0:
+    # if number is multiple of 3 and 5
+        print("Danish Khan")
+    elif x % 3 == 0:
+    # if number is multiple of 3
+        print("Danish")
+    elif x % 5 == 0:
+    # if number is multiple of 5
+        print("Khan")
+    else:
+    # otherwise print number
+        print("Hello!")
